a35d5a99e0ac4a2c785db583a1bbf247626ed446
kolchanov <kolchanov@siberian.pro>
2021-06-28 05:38:58 +0000

    Некоторое изменение модели взаимодействия

--- model/Layout.js
+++ model/Layout.js
@@ -2,8 +2,9 @@
 
 const schema = new Schema({
     user: { type: Object, unique: false, required: true },
-    message: { type: Object, unique: false, required: true },
-    chatId: { type: Number, unique: true, required: true }
+    messageId: { type: Number, unique: false, required: true },
+    chatId: { type: Number, unique: false, required: true },
+    username: { type: Object, unique: false, required: true }
 })
 
 module.exports = model('Layout', schema)
