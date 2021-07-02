a1c52f37cad6894e79d1c9cc0f85eecd9a54c6b3
kolchanov <kolchanov@siberian.pro>
2021-07-02 15:57:09 +0000

    Модели для банов и админов

---
+++ model/AdminUsers.js
@@ -0,0 +1,7 @@
+﻿const { Schema, model } = require('mongoose')
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true }
+})
+
+module.exports = model('Layout', schema)
---
+++ model/BanUsers.js
@@ -0,0 +1,6 @@
+﻿const { Schema, model } = require('mongoose')
+
+const schema = new Schema({ username: { type: String, required: true, unique: true } })
+// user_id: { type: String || Number, required: true, unique: true }
+
+module.exports = model('BanUsers', schema)
